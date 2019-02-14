29d5d48e9390419e473302c2181957be8560b1f6
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2019-02-14 17:49:53 +0530

    gighest word from file

--- src/main/java/com/interviewQuestions/HighestTypedWord.java
+++ src/main/java/com/interviewQuestions/HighestTypedWord.java
@@ -1,6 +1,11 @@
 package com.interviewQuestions;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -9,13 +14,60 @@ import java.util.Set;
 
 public class HighestTypedWord {
 	private static Scanner sc;
+	private static int i = 0;
+	private static String word = null;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		sc = new Scanner(System.in);
-		int i = 0;
-		String word = null;
-		System.out.println("Enter a sentense to detect repeated words");
-		String sentense = sc.nextLine();
+		int choice = 0;
+		boolean repeatflag = false;
+		do {
+			System.out.println("Enter your choice 1 for file 0 for sentense");
+			try {
+				choice = sc.nextInt();
+			} catch (InputMismatchException ex) {
+				System.out.println("enter valid choice");
+			}
+
+			switch (choice) {
+			case 0:
+				System.out.println("Enter a sentense to detect repeated words");
+				sc = new Scanner(System.in);
+				String sentense = sc.nextLine();
+				findRepeatsForSentense(sentense);
+				break;
+			case 1:
+				System.out.println("please enter path to your file");
+				sc = new Scanner(System.in);
+				FileInputStream fif = null;
+				String path = sc.nextLine();
+				try {
+					fif = new FileInputStream(new File(path));
+					int i = 0;
+					StringBuilder sentensefromfile = new StringBuilder();
+					while ((i = fif.read()) != -1) {
+						sentensefromfile.append((char) i);
+					}
+					findRepeatsForSentense(sentensefromfile.toString());
+				} catch (FileNotFoundException ex) {
+					System.out.println("Invalid file path");
+				}
+
+				break;
+			default:
+				System.out.println("invalid choice");
+			}
+			sc=new Scanner(System.in);
+			System.out.println("Enter 'Y' or 'y' to continue");
+			repeatflag=sc.nextLine().equalsIgnoreCase("Y")?true:false;
+		} while (repeatflag);
+
+		// System.out.println("Enter a sentense to detect repeated words");
+		// String sentense = sc.nextLine();
+
+	}
+
+	private static void findRepeatsForSentense(String sentense) {
 		String[] words = sentense.split("\\s+");
 		Map<String, Integer> result = detectRepeatedWords(words);
 		Set<Entry<String, Integer>> entry = result.entrySet();
@@ -24,14 +76,13 @@ public class HighestTypedWord {
 			Map.Entry<String, Integer> e = iterator.next();
 			if (e.getValue() > i) {
 				i = e.getValue();
-				word=e.getKey();
+				word = e.getKey();
 			}
-
 		}
-		if(i>1)
-		System.out.println("you have typed " + word+ " word "+i+" number of times");
+		if (i > 1)
+			System.out.println("you have typed " + word + " word " + i + " number of times");
 		else
-		System.out.println("there is no repeated word in your sentense");
+			System.out.println("there is no repeated word in your sentense");
 
 	}
 
@@ -43,9 +94,7 @@ public class HighestTypedWord {
 			} else {
 				result.put(words[i], 1);
 			}
-
 		}
 		return result;
 	}
-
 }
