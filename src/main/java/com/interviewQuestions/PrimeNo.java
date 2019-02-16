cb36cac8c97ecf81fa58d76326300e23ab6682b0
Rasmiranjan Rath <rasmiranjan.rath@girmiti.com>
2019-02-16 12:57:14 +0530

    prime no

---
+++ src/main/java/com/interviewQuestions/PrimeNo.java
@@ -0,0 +1,26 @@
+package com.interviewQuestions;
+
+import java.util.Scanner;
+
+public class PrimeNo {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a number");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+
+		for (int i = 1; i < number; i++) {
+			boolean isPrime = true;
+			for (int j = 2; j < i; j++) {
+				if (i % j == 0) {
+					isPrime = false;
+					break;
+				}
+			}
+			if (isPrime)
+				System.out.print(i + " ");
+		}
+
+	}
+
+}
